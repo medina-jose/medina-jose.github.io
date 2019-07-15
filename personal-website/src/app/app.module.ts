a7ada6fb0ca58fb1005e4c388da27c7b9c179a7d
medina-jose <jose.medina2073@gmail.com>
2019-07-14 21:08:57 -0400

    initial commit for homepage

--- personal-website/src/app/app.module.ts
+++ personal-website/src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-// routing dependencies
+// routing 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 // other dependencies
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap'; 
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { HomeComponent } from './home/home.component'; 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
